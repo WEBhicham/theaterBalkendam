c1f66a6e5d6b128585df7133beb3a98c025caf92
WEBhicham <allatrona@gmail.com>
2017-06-01 09:48:19 +0200

    Standard things
    let's go!

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>Theater Balkendam</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="Assets/StyleSheets/general.css">
+        <script src="Assets/JavaScript/general.js" defer></script>
+    </head>
+    <body>
+        <h1>Hello world!</h1>
+    </body>
+</html> 
